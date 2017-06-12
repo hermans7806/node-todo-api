6eb7525a8f523fc70cf7de62137841a2f95a22d0
Herman Sutanto <herman.sutanto@reebonz.com>
2017-06-12 18:20:58 +0800

    add queries playground file

---
+++ playground/mangoose-queries.js
@@ -0,0 +1,37 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {Users} = require('./../server/models/users');
+
+// var id = '593e62c5bf2b86e30bc79a671';
+//
+// if (!ObjectID.isValid(id)) {
+//   return console.log('ID not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('ID not found');
+//   };
+//   console.log('Todo by ID', todo);
+// }).catch((e) => console.log(e));
+
+Users.findById('593e696ac6a4db5f95fd4f5a').then((user) => {
+  if (!user) {
+    return console.log('User Not Found');
+  };
+  console.log('User by ID', user);
+}).catch((e) => console.log(e));
